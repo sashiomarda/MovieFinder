7c5d8e78443cc88d8e68273b523e11647362ec42
sashiomarda <56986990+sashiomarda@users.noreply.github.com>
2021-08-30 21:12:08 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,4 +3,20 @@ import "./styles/style.css";
 import "./script/component/app-bar.js";
 import main from "./script/view/main.js";
 
+
+const displayTime = () => {
+    moment.locale("id");
+    $(".time").text(moment().format("LTS"));
+    $(".date").text(moment().format("LL"));
+    console.log("tesssmain");
+    console.log(moment().format("LTS"));
+};
+
+const updateTime = () => {
+    displayTime();
+    setTimeout(updateTime, 1000)
+};
+
+updateTime();
+
 document.addEventListener("DOMContentLoaded", main);
